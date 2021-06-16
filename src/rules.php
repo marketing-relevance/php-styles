a014b5244f8bcab507e27cc2d26a6e3da6eee97a
Eric Pfeiffer <63664924+ericp-mrel@users.noreply.github.com>
2021-06-16 12:22:19 -0500

    Update rules.php

--- src/rules.php
+++ src/rules.php
@@ -4,23 +4,13 @@
 
 return [
     'array_syntax' => ['syntax' => 'short'],
-    'binary_operator_spaces' => [
-        'default' => 'single_space',
-        'operators' => ['=>' => null]
-    ],
+
     'blank_line_after_namespace' => true,
     'blank_line_after_opening_tag' => true,
-    'blank_line_before_statement' => [
-        'statements' => ['return']
-    ],
     'braces' => true,
     'cast_spaces' => true,
-    'class_attributes_separation' => [
-        'elements' => ['method']
-    ],
-    'class_definition' => true,
     'concat_space' => [
-        'spacing' => 'none'
+        'spacing' => 'none',
     ],
     'declare_equal_normalize' => true,
     'elseif' => true,
@@ -36,7 +26,6 @@ return [
     'linebreak_after_opening_tag' => true,
     'line_ending' => true,
     'lowercase_cast' => true,
-    'lowercase_constants' => true,
     'lowercase_keywords' => true,
     'lowercase_static_reference' => true, // added from Symfony
     'magic_method_casing' => true, // added from Symfony
@@ -50,7 +39,7 @@ return [
             'throw',
             'use',
             'use_trait',
-        ]
+        ],
     ],
     'no_blank_lines_after_class_opening' => true,
     'no_blank_lines_after_phpdoc' => true,
@@ -60,22 +49,20 @@ return [
     'no_leading_import_slash' => true,
     'no_leading_namespace_whitespace' => true,
     'no_mixed_echo_print' => [
-        'use' => 'echo'
+        'use' => 'echo',
     ],
     'no_multiline_whitespace_around_double_arrow' => true,
     'multiline_whitespace_before_semicolons' => [
-        'strategy' => 'no_multi_line'
+        'strategy' => 'no_multi_line',
     ],
     'no_short_bool_cast' => true,
     'no_singleline_whitespace_before_semicolons' => true,
     'no_spaces_after_function_name' => true,
-    'no_spaces_around_offset' => true,
     'no_spaces_inside_parenthesis' => true,
     'no_trailing_comma_in_list_call' => true,
     'no_trailing_comma_in_singleline_array' => true,
     'no_trailing_whitespace' => true,
     'no_trailing_whitespace_in_comment' => true,
-    'no_unneeded_control_parentheses' => true,
     'no_unreachable_default_argument_value' => true,
     'no_useless_return' => true,
     'no_whitespace_before_comma_in_array' => true,
@@ -83,9 +70,7 @@ return [
     'normalize_index_brace' => true,
     'not_operator_with_successor_space' => true,
     'object_operator_without_whitespace' => true,
-    'ordered_imports' => ['sortAlgorithm' => 'alpha'],
     'phpdoc_indent' => true,
-    'phpdoc_inline_tag' => true,
     'phpdoc_no_access' => true,
     'phpdoc_no_package' => true,
     'phpdoc_no_useless_inheritdoc' => true,
@@ -96,17 +81,15 @@ return [
     'phpdoc_trim' => true,
     'phpdoc_types' => true,
     'phpdoc_var_without_name' => true,
-    'psr4' => true,
     'self_accessor' => true,
     'short_scalar_cast' => true,
     'simplified_null_return' => false, // disabled by Shift
     'single_blank_line_at_eof' => true,
     'single_blank_line_before_namespace' => true,
-    'single_class_element_per_statement' => true,
     'single_import_per_statement' => true,
     'single_line_after_imports' => true,
     'single_line_comment_style' => [
-        'comment_types' => ['hash']
+        'comment_types' => ['hash'],
     ],
     'single_quote' => true,
     'space_after_semicolon' => true,
@@ -114,11 +97,52 @@ return [
     'switch_case_semicolon_to_colon' => true,
     'switch_case_space' => true,
     'ternary_operator_spaces' => true,
-    'trailing_comma_in_multiline_array' => true,
     'trim_array_spaces' => true,
     'unary_operator_spaces' => true,
-    'visibility_required' => [
-        'elements' => ['method', 'property']
-    ],
     'whitespace_after_comma_in_array' => true,
+
+    // php-cs-fixer 3: Renamed rules
+    'constant_case' => ['case' => 'lower'],
+    'general_phpdoc_tag_rename' => true,
+    'phpdoc_inline_tag_normalizer' => true,
+    'phpdoc_tag_type' => true,
+    'psr_autoloading' => true,
+    'trailing_comma_in_multiline' => ['elements' => ['arrays']],
+
+    // php-cs-fixer 3: Changed options
+    'binary_operator_spaces' => [
+        'default' => 'single_space',
+        'operators' => ['=>' => null],
+    ],
+    'blank_line_before_statement' => [
+        'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
+    ],
+    'class_attributes_separation' => [
+        'elements' => [
+            'const' => 'one',
+            'method' => 'one',
+            'property' => 'one',
+        ],
+    ],
+    'class_definition' => [
+        'multi_line_extends_each_single_line' => true,
+        'single_item_single_line' => true,
+        'single_line' => true,
+    ],
+    'ordered_imports' => [
+        'sort_algorithm' => 'alpha',
+    ],
+    'no_unused_imports' => true,
+
+    // php-cs-fixer 3: Removed rootless options (*)
+    'no_unneeded_control_parentheses' => [
+        'statements' => ['break', 'clone', 'continue', 'echo_print', 'return', 'switch_case', 'yield'],
+    ],
+    'no_spaces_around_offset' => [
+        'positions' => ['inside', 'outside'],
+    ],
+    'visibility_required' => [
+        'elements' => ['property', 'method', 'const'],
+    ],
+    'single_trait_insert_per_statement' => true,
 ];
